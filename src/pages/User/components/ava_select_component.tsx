ab86f8342cc2c3fdf9e01a0620ee81ed0f2abaf6
mddd <413153189@qq.com>
2021-10-02 15:03:49 +0800

    新增添加用户的页面

--- src/pages/User/components/ava_select_component.tsx
+++ src/pages/User/components/ava_select_component.tsx
@@ -1,18 +1,68 @@
-import { Card } from 'antd';
-import React from 'react';
+import { Avatar, Space } from 'antd';
+import React, { useState } from 'react';
 import { useMount } from '@umijs/hooks';
 import Api from '@/utils/request';
+import { SystemPicter } from 'dd_server_api_web/apis/model/avater';
+import { successResultHandle } from 'dd_server_api_web/apis/utils/ResultUtil';
 
-const AvatorPictorSelect: React.FC = () => {
+/**
+ * 系统默认头像选择器
+ * @param current 选中的头像
+ * @param onSelect 选中执行的操作
+ * @constructor
+ */
+const AvatorPictorSelect: React.FC<{
+  current?: SystemPicter;
+  onSelect?: (avatar: SystemPicter) => void;
+}> = ({ current, onSelect }) => {
+  const [pics, setPics] = useState<SystemPicter[]>([]);
+
   useMount(() => {
     Api.getInstance()
       .getPics(1)
       .then((value) => {
-        console.log(value);
+        successResultHandle<SystemPicter[]>(value, (data) => {
+          setPics(data);
+        });
       });
   });
 
-  return <Card></Card>;
+  return (
+    <Space>
+      {pics.map((value) => {
+        return (
+          <div
+            style={{
+              position: 'relative',
+            }}
+            key={value.id}
+            onClick={() => {
+              if (onSelect) {
+                onSelect(value);
+              }
+            }}
+          >
+            <Avatar src={value.url} size={64} />
+
+            {current && current.id == value.id && (
+              <div
+                style={{
+                  position: 'absolute',
+                  top: 0,
+                  left: 0,
+                  width: 64,
+                  height: 64,
+                  borderRadius: '50%',
+                  background: 'grey',
+                  opacity: 0.5,
+                }}
+              />
+            )}
+          </div>
+        );
+      })}
+    </Space>
+  );
 };
 
 export default AvatorPictorSelect;
--- src/pages/User/list/AddNewUser.tsx
+++ src/pages/User/list/AddNewUser.tsx
@@ -1,20 +1,87 @@
 import { PageContainer } from '@ant-design/pro-layout';
-import React from 'react';
-import { Card, Form, Input } from 'antd';
+import React, { useState } from 'react';
+import { Button, Card, Form, Input, message, Typography } from 'antd';
 import AvatorPictorSelect from '@/pages/User/components/ava_select_component';
+import { SystemPicter } from 'dd_server_api_web/apis/model/avater';
+import Api from '@/utils/request';
+import { responseIsSuccess } from 'dd_server_api_web/apis/utils/ResultUtil';
 
 const AddNewUser: React.FC = () => {
+  const [avatar, setAvatar] = useState<SystemPicter>();
+  const [registarIng, setRegistarIng] = useState<boolean>(false);
+
+  const submit = async (values: any) => {
+    console.log(values);
+    if (values.password != values.repassword) {
+      message.error('两次输入密码不一致');
+      return;
+    }
+    if (!avatar) {
+      message.error('请选择头像');
+      return;
+    }
+
+    setRegistarIng(true);
+    const result = await Api.getInstance().register(
+      values.loginName,
+      values.password,
+      avatar?.url ?? '',
+    );
+    if (responseIsSuccess(result)) {
+      message.success(result.message);
+    } else {
+      message.error(result.message);
+    }
+    setRegistarIng(false);
+  };
+
   return (
     <PageContainer>
       <Card>
-        <Form>
-          <AvatorPictorSelect />
+        <Form layout={'vertical'} onFinish={submit}>
+          <div
+            style={{
+              textAlign: 'center',
+              marginBottom: 20,
+            }}
+          >
+            <Typography>设置用户头像</Typography>
+            <AvatorPictorSelect current={avatar} onSelect={setAvatar} />
+          </div>
           <Form.Item label={'用户名'} name={'loginName'}>
             <Input />
           </Form.Item>
 
-          <Form.Item label={'输入登录密码'} name={'password'}>
-            <Input />
+          <Form.Item
+            label={'输入登录密码'}
+            name={'password'}
+            rules={[
+              {
+                required: true,
+                message: '请输入登录密码',
+              },
+            ]}
+          >
+            <Input type={'password'} />
+          </Form.Item>
+
+          <Form.Item
+            label={'再确认一遍密码'}
+            name={'repassword'}
+            rules={[
+              {
+                required: true,
+                message: '请再次输入密码',
+              },
+            ]}
+          >
+            <Input type={'password'} />
+          </Form.Item>
+
+          <Form.Item>
+            <Button loading={registarIng} type={'primary'} htmlType={'submit'}>
+              创建新帐号
+            </Button>
           </Form.Item>
         </Form>
       </Card>
